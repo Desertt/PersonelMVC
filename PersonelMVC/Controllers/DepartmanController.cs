43c3155c7413a1dfe19db3ae8a247fd0fdc06189
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2019-09-19 17:31:09 +0300

    PersonelMVC

---
+++ PersonelMVC/Controllers/DepartmanController.cs
@@ -0,0 +1,20 @@
+﻿using PersonelMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonelMVC.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        //domain/Departman/Merhaba
+      public ActionResult Merhaba()
+        {
+            var departman = new Departman() { Id =1,Ad="Bilgi İşlem" };
+            return View();
+
+        }
+    }
+}
