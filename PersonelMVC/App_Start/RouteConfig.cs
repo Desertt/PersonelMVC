f66e31473a5c14a9dbddb4ac4e52b92da0c0bdd1
Mustafa DAĞTEKİN <mustafa.dagtekin@gmail.com>
2019-09-24 02:59:36 +0300

    Route Attribute

--- PersonelMVC/App_Start/RouteConfig.cs
+++ PersonelMVC/App_Start/RouteConfig.cs
@@ -12,6 +12,21 @@ namespace PersonelMVC
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //Kendi URL mizi oluşturalım...
+
+            routes.MapMvcAttributeRoutes();  //--Route Attribute Kullanımı Daha Faydalıdır.Büyüyen RouteConfig sayfasını karmaşıklığını çözer.
+
+            //routes.MapRoute(
+            //    name: "Anasayfa",
+            //    url: "",
+            //    defaults: new { controller = "Departman", action = "Merhaba"}
+            //);
+
+            //routes.MapRoute(
+            //    name: "Personel",
+            //    url: "personel/liste/{siralama}/{sayfa}",
+            //    defaults: new { controller = "Personel", action = "PersonelListesi" }
+            //);
 
             routes.MapRoute(
                 name: "Default",
--- PersonelMVC/Controllers/PersonelController.cs
+++ PersonelMVC/Controllers/PersonelController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace PersonelMVC.Controllers
 {
@@ -12,6 +8,12 @@ namespace PersonelMVC.Controllers
         public ActionResult Index()
         {
             return Content("Personel Anasayfa");
+        }
+
+        [Route("personel/liste/{siralama}/{sayfa}")]
+        public ActionResult PersonelListesiGetir(string siralama, int sayfa)
+        {
+            return Content(siralama + "-" + sayfa);
         }
     }
 }
